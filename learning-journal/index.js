2ba1d71046047197f0d6606aa06a520d4c569db5
ycheryl <yulcheryl2357@gmail.com>
2023-08-07 17:27:28 +0700

    update file

--- learning-journal/index.js
+++ learning-journal/index.js
@@ -1,11 +1,13 @@
-import { blogs, first } from './data.js'
+import { blogs } from './data.js'
 const mainBlog = document.getElementById('main-blog')
 const blogContainer = document.getElementById('blog-container')
 const viewMore = document.getElementById('view-more')
 
 function renderMainBlog() {
     let blogHTML = ``
-    first.forEach(function(blog){
+    let main = []
+    main.push(blogs[0])
+    main.forEach(function(blog){
         blogHTML += `
         <p class="blog-date">${blog.date}</p>
         <h1 class="blog-title">${blog.title}</h1>
@@ -16,9 +18,9 @@ function renderMainBlog() {
 }
 
 function renderBlog() {
-    let partialBlog = []
-    partialBlog.push(blogs[0], blogs[1], blogs[2])
     let blogHTML = ``
+    let partialBlog = []
+    partialBlog.push(blogs[1], blogs[2], blogs[3])
 
     partialBlog.forEach(function(blog){
         blogHTML += `
@@ -34,13 +36,17 @@ function renderBlog() {
     blogContainer.innerHTML = blogHTML
 }
 
-renderMainBlog()
 renderBlog()
+renderMainBlog()
 
 function renderAllBlogs() {
     let blogHTML = ``
+    let all = []
+    for (let i = 1; i < blogs.length; i++) {
+        all.push(blogs[i])
+    }
 
-    blogs.forEach(function(blog){
+    all.forEach(function(blog){
         blogHTML += `
         <div class="blog">
             <img src="${blog.image}">
@@ -48,7 +54,7 @@ function renderAllBlogs() {
             <h2 class="blog-title">${blog.title}</h2>
             <p class="blog-desc">${blog.desc}</p>
         </div>
-        `
+        ` 
     })
 
     blogContainer.innerHTML = blogHTML
