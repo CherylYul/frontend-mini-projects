b7ef3f2cb584dce019ce19e734f730b59b81ab43
ycheryl <yulcheryl2357@gmail.com>
2023-09-25 22:59:50 +0700

    update file

--- index.jsx
+++ index.jsx
@@ -2,18 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Menu from "./components/Menu/index"
 import Star from "./components/Star"
+import Badges from "./components/Badges/Badges"
+import Notification from './components/Notifications/index';
 
 function App() {
   return (
-      <Menu onOpen={() => console.log("Opened/closed")}>
-        <Menu.Button>Menu</Menu.Button>
-        <Menu.Dropdown>
-          <Menu.Item>Home</Menu.Item>
-          <Menu.Item>About</Menu.Item>
-          <Menu.Item>Contact</Menu.Item>
-          <Menu.Item>Blog</Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
+      <div>
+        <div>
+          <Menu onOpen={() => console.log("Opened/closed")}>
+          <Menu.Button>Menu</Menu.Button>
+          <Menu.Dropdown>
+            <Menu.Item>Home</Menu.Item>
+            <Menu.Item>About</Menu.Item>
+            <Menu.Item>Contact</Menu.Item>
+            <Menu.Item>Blog</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+        </div>
+        <hr />
+        <div>
+          <Badges className="badge" size="lg" color="red" shape="square">
+            badges
+          </Badges>
+        </div>
+        <hr />
+        <div>
+          <Notification className="noti" status="success">
+            <Notification.Title>Congratulations!</Notification.Title>
+            <Notification.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Notification.Text>
+          </Notification>
+        </div>
+        <hr />
+        <div>
+          <Notification className="noti" status="warning">
+            <Notification.Title>Attention!</Notification.Title>
+            <Notification.Text></Notification.Text>
+          </Notification>
+        </div>
+        <hr />
+        <div>
+          <Notification className="noti" status="danger">
+            <Notification.Title>There is a problem with your application</Notification.Title>
+            <Notification.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.</Notification.Text>
+          </Notification>
+        </div>
+        <hr />
+        <div>
+          <Notification className="noti" status="info">
+            <Notification.Title>Update available</Notification.Title>
+            <Notification.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Notification.Text>
+          </Notification>
+        </div>
+      </div>
   )
 }
 
