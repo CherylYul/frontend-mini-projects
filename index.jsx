2944658d2b59f00fd3514d29ed1ccb28543d07a9
ycheryl <yulcheryl2357@gmail.com>
2023-10-04 23:11:40 +0700

    update file

--- index.jsx
+++ index.jsx
@@ -4,6 +4,8 @@ import Menu from "./components/Menu/index"
 import Star from "./components/Star"
 import Badges from "./components/Badges/Badges"
 import Notification from './components/Notifications/index';
+import Card from "./components/Cards/index"
+import Avatar from "./components/Avatar/Avatar"
 
 function App() {
   return (
@@ -19,39 +21,71 @@ function App() {
           </Menu.Dropdown>
         </Menu>
         </div>
-        <hr />
+        <br />
+        <br />
         <div>
           <Badges className="badge" size="lg" color="red" shape="square">
             badges
           </Badges>
         </div>
-        <hr />
+        <br />
+        <br />
         <div>
           <Notification className="noti" status="success">
             <Notification.Title>Congratulations!</Notification.Title>
             <Notification.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Notification.Text>
           </Notification>
         </div>
-        <hr />
+        <br />
+        <br />
         <div>
           <Notification className="noti" status="warning">
             <Notification.Title>Attention!</Notification.Title>
             <Notification.Text></Notification.Text>
           </Notification>
         </div>
-        <hr />
+        <br />
+        <br />
         <div>
           <Notification className="noti" status="danger">
             <Notification.Title>There is a problem with your application</Notification.Title>
             <Notification.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.</Notification.Text>
           </Notification>
         </div>
-        <hr />
+        <br />
+        <br />
         <div>
           <Notification className="noti" status="info">
             <Notification.Title>Update available</Notification.Title>
             <Notification.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Notification.Text>
           </Notification>
+        </div>
+        <br />
+        <br />
+        <div>
+          <Card>
+            <Card.Icon><i class="bi bi-cloud-upload"></i></Card.Icon>
+            <Card.Title>Easy Deployment</Card.Title>
+            <Card.Text>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</Card.Text>
+          </Card>
+        </div>
+        <br />
+        <br />
+        <div>
+          <Card>
+            <Card.Icon><i class="bi bi-house-door"></i></Card.Icon>
+            <Card.Title>Safe House</Card.Title>
+            <Card.Text>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</Card.Text>
+          </Card>
+        </div>
+        <br />
+        <br />
+        <div>
+          <Avatar src="./components/Avatar/images/bob.jpg" alt="Bob Ziroll" />
+          <br />
+          <Avatar variant="letters">BZ</Avatar>
+          <br />
+          <Avatar variant="icon" />
         </div>
       </div>
   )
