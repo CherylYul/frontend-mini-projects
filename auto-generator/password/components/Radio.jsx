41c427e5fb505960472b173d95b2dd43b7db4efa
ycheryl <yulcheryl2357@gmail.com>
2023-12-14 23:33:38 +0700

    add radio

--- auto-generator/password/components/Radio.jsx
+++ auto-generator/password/components/Radio.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import { PasswordContext } from "../App"
 
-export default function Radio({children, choiceGroup}) {
+export default function Radio({children}) {
+    const { form, handleChange } = React.useContext(PasswordContext)
+
     return (
         <div className="radio">
-            <input type="radio" name={choiceGroup} value={children}/>
+            <input type="radio" 
+                    onChange={handleChange}
+                    name={form.Requirements} 
+                    value={children} 
+                    id={children} 
+                    checked={form.Requirements === children} />
             <label htmlFor={children}>{children}</label>
         </div>
     )
