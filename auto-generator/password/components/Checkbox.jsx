dcb75737c7a0c5f3d921bf5db2613eacab11adb6
ycheryl <yulcheryl2357@gmail.com>
2023-12-14 23:33:01 +0700

    add checkbox

--- auto-generator/password/components/Checkbox.jsx
+++ auto-generator/password/components/Checkbox.jsx
@@ -1,10 +1,16 @@
 import React from "react"
+import { PasswordContext } from "../App"
 
-export default function Checkbox({children}) {
+export default function Checkbox({children, check}) {
+    const { form, handleChange } = React.useContext(PasswordContext)
     return (
         <div className="check-box">
-            <input type="checkbox" />
-            <label>{children}</label>
+            <input type="checkbox"
+                    onChange={handleChange}
+                    name={children}
+                    checked={check}
+                    id={children} />
+            <label htmlFor={children}>{children}</label>
         </div>
     )
 }
