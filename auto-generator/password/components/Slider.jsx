96de58e7f024032aacac7b930b43c3ac855dda01
ycheryl <yulcheryl2357@gmail.com>
2023-12-14 23:33:55 +0700

    add slider

--- auto-generator/password/components/Slider.jsx
+++ auto-generator/password/components/Slider.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { PasswordContext  } from "../App"
 
 export default function Slider({children}) {
+    const { form, handleChange } = React.useContext(PasswordContext)
     return (
-        <div className="slider">
+        <div className="password-length">
             <label htmlFor={children}>{children}</label>
-            <input type="number" min="1" max="50" defaultValue="12" />
-            <input type="range" min="1" max="50" defaultValue="12" />
+            <input type="number" min="1" max="50"
+                    value={form.Length} 
+                    name="Length" 
+                    onChange={handleChange} />
+            <input type="range" min="1" max="50"
+                    value={form.Length}
+                    name="Length" 
+                    onChange={handleChange} />
         </div>
     )
 }
