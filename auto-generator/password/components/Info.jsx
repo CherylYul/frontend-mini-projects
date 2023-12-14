8a0f396a92d4c2e420601e69f141f18f5946b00c
ycheryl <yulcheryl2357@gmail.com>
2023-12-14 23:33:17 +0700

    add info

--- auto-generator/password/components/Info.jsx
+++ auto-generator/password/components/Info.jsx
@@ -1,3 +1,13 @@
 import React from "react"
 
-
+export default function Info({children}) {
+    const styles = {
+        display: "none",
+    }
+    return (
+        <div className="info">
+            <i className="bi bi-info-circle-fill tooltip"></i>
+            <div className="info-details" >{children}</div>
+        </div>
+    )
+}
