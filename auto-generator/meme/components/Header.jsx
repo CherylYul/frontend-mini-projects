af5b11d0fd186ad274ab91efeda8a2546b385da8
ycheryl <yulcheryl2357@gmail.com>
2024-01-03 16:58:20 +0700

    update meme generator

---
+++ auto-generator/meme/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <i className="bi bi-emoji-heart-eyes-fill"></i>
+            <h3 className="header--title">Meme Generator</h3>
+            <h4 className="header--name">MemeHub</h4>
+        </header>
+    )
+}
