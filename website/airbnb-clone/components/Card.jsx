90097964e0b86f260d885350603407e2160fda83
ycheryl <yulcheryl2357@gmail.com>
2024-01-14 19:09:11 +0700

    update project

---
+++ website/airbnb-clone/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Card({title, price, coverImg, stats, location, openSpots}) {
+    let badgeText = openSpots === 0 ? "SOLD OUT" :
+                    location === "Online" ? "ONLINE" : ""
+    
+    return (
+        <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${coverImg}`} className="card--image" />
+            <div className="card--stats">
+                <i className="bi bi-star-fill"></i>
+                <span>{stats.rating}</span>
+                <span className="gray"> ({stats.reviewCount}) • </span>
+                <span className="gray">{location}</span>
+            </div>
+            <p className="card--title">{title}</p>
+            <p className="card--price">
+                <span className="bold">From ${price}</span> / person
+            </p>
+        </div>
+    )
+}
---
+++ website/coffee-shop/data.js
@@ -0,0 +1,34 @@
+export const menuArray = [
+    {
+        name: "Capuchino",
+        ingredients: "expresso, steamed milk, foamed milk",
+        price: 4.36,
+        quantity: 0,
+        image: "capuchino.png",
+        id: 0
+    },
+    {
+        name: "Latte",
+        ingredients: "expresso, steamed milk, foamed milk",
+        price: 4.23,
+        quantity: 0,
+        image: "latte.png",
+        id: 1
+    },
+    {
+        name: "Cookie Ice Blended",
+        ingredients: "cookie, oreo, ice, creamcheese, cacao",
+        price: 5.19,
+        quantity: 0,
+        image: "cookie-ice-blended.png",
+        id: 2
+    },
+    {
+        name: "Tiramisu",
+        ingredients: "cream, coffee, vanilla, cacao powder, sugar, egg",
+        price: 5.59,
+        quantity: 0,
+        image: "tiramisu.png",
+        id: 3
+    },
+]
