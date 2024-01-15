bb9193af03a646eaf1974173e8781ed54a72301c
ycheryl <yulcheryl2357@gmail.com>
2024-01-15 13:16:25 +0700

    add pages

---
+++ website/coffee-shop/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Hero from "../components/Hero"
+import Shop from "../components/Shop"
+import data from "../data"
+
+export default function ShopDetail() {
+    return (
+        <div>
+            <Hero />
+            <div className="center-container">
+                <h1 className="center">ADELAIDE’S BEST COFFEE SHOPS</h1>
+                {data.map(shop => <Shop key={shop.id} {...shop} />)}
+            </div>
+        </div>
+    )
+}
---
+++ website/coffee-shop/pages/ShopDetail.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+// {
+//     id: 1,
+//     title: "SUMMERTOWN STUDIO",
+//     description: "A seaside hotspot known amongst  locals for its delicious coffee.  While an old warehouse, surrounded by car dealerships, is not the obvious place for a coffee shop - Summertown’s laid-back ambience and vintage décor makes it feel as though you’re simply stopping by your mates place for a cuppa.",
+//     price: 18,
+//     coverImg: "summertownstudio.png",
+//     stats: {
+//         rating: 5.0,
+//         reviewCount: 120,
+//     },
+//     location: "13 Paringa Avenue, Somerton Park",
+//     facilities: ["Alfresco/Outdoor Dining", "Entertainment", "Non Smoking", "Pop Up Wine Bar", "Wine Tasting", "Bar", "Online Orders", "Shop / Gift Shop", "Cooking School", "Live Music", "Pet Friendly - Enquire", "Takeaway Available"],
+//     menu: [
+//         {
+//             name: "Cappuccino",
+//             image: "summerstudio-cappuccino.png",
+//             ingredients: "expresso, steamed mil foam",
+//             price: 6,
+//         }
+//     ]
+// }
+export default function ShopDetail() {
+    return (
+        <div>
+            <div className="center-container">
+                
+                Shop here!
+                
+            </div>
+        </div>
+    )
+}
