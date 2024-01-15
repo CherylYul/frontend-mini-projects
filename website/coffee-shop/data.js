faa02f7bbb39ad22bcf6e0b6a7dbd97b20f8404e
ycheryl <yulcheryl2357@gmail.com>
2024-01-15 13:15:09 +0700

    update data

--- website/coffee-shop/data.js
+++ website/coffee-shop/data.js
@@ -1,34 +1,118 @@
-export const menuArray = [
+export default [
     {
-        name: "Capuchino",
-        ingredients: "expresso, steamed milk, foamed milk",
-        price: 4.36,
-        quantity: 0,
-        image: "capuchino.png",
-        id: 0
+        id: 1,
+        title: "SUMMERTOWN STUDIO",
+        description: "A seaside hotspot known amongst  locals for its delicious coffee.  While an old warehouse, surrounded by car dealerships, is not the obvious place for a coffee shop - Summertown’s laid-back ambience and vintage décor makes it feel as though you’re simply stopping by your mates place for a cuppa.",
+        coverImg: "summerstudio.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 120,
+        },
+        location: "13 Paringa Avenue, Somerton Park",
+        facilities: ["Alfresco/Outdoor Dining", "Entertainment", "Non Smoking", "Pop Up Wine Bar", "Wine Tasting", "Bar", "Online Orders", "Shop / Gift Shop", "Cooking School", "Live Music", "Pet Friendly - Enquire", "Takeaway Available"],
+        menu: [
+            {
+                name: "Cappuccino",
+                image: "summerstudio-cappuccino.png",
+                ingredients: "expresso, steamed mil foam",
+                price: 6,
+            }
+        ]
     },
     {
-        name: "Latte",
-        ingredients: "expresso, steamed milk, foamed milk",
-        price: 4.23,
-        quantity: 0,
-        image: "latte.png",
-        id: 1
+        id: 2,
+        title: "COFFYLOSOPHY",
+        description: "An award-winning café serving up some of the best coffee Adelaide has to offer. Rhe intimate atmosphere of Coffylosphy paired with its friendly baristas makes for a welcoming space to enjoy your morning, or afternoon, cup of joe.",
+        coverImg: "coffylosophy.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 293,
+        },
+        location: "198A Hutt Street",
+        facilities: ["Alfresco/Outdoor Dining", "Non Smoking", "Takeaway Available", "Delivery available", "Online Orders", "Family Friendly", "Pet Friendly - Enquire"],
+        menu: [
+            {
+                name: "Big Breakfast",
+                image: "big-breakfast.png",
+                ingredients: "2 eggs your way, steaky bacon, sausage, baked tonnato, teter tots, herbed mushroom, wilted greens, sourdough",
+                price: 30
+            },
+            {
+                name: "Cafe Latte",
+                image: "latte.png",
+                ingredients: "expresso, steamed milk",
+                price: 6,
+            },
+            {
+                name: "Cappuccino",
+                image: "cappuccino.png",
+                ingredients: "expresso, steamed mil foam",
+                price: 6,
+            }
+        ]
     },
     {
-        name: "Cookie Ice Blended",
-        ingredients: "cookie, oreo, ice, creamcheese, cacao",
-        price: 5.19,
-        quantity: 0,
-        image: "cookie-ice-blended.png",
-        id: 2
+        id: 3,
+        title: "PETER RABBIT",
+        description: "The café boasting lush gardens filled with lemon and avocado trees, pair your coffee with something sweet thanks to the daily pastry selection baked in-house, or linger a little longer and experience one of the best breakfasts Adelaide has to offer.",
+        coverImg: "peter-rabbit.png",
+        stats: {
+            rating: 4.6,
+            reviewCount: 224,
+        },
+        location: "244 Hindley Street",
+        facilities: ["Alfresco/Outdoor Dining", "Suitable for Functions", "Bar", "Takeaway Available", "Pet Friendly - Enquire"],
+        menu: [
+            {
+                name: "Mimosa",
+                image: "mimosa.png",
+                ingredients: "prosecco, orange juice",
+                price: "16",
+            },
+            {
+                name: "Gin & Juice",
+                image: "gin-and-juice.png",
+                ingredients: "Adelaide Gin, seasonal juice",
+                price: "18",      
+            },
+            {
+                name: "Matcha Martini",
+                image: "matcha-martini.png",
+                ingredients: "Adelaide Vodka, Amaretto, matcha, almond, honey",
+                price: "20",      
+            }
+        ]
     },
     {
-        name: "Tiramisu",
-        ingredients: "cream, coffee, vanilla, cacao powder, sugar, egg",
-        price: 5.59,
-        quantity: 0,
-        image: "tiramisu.png",
-        id: 3
+        id: 4,
+        title: "BLOOM",
+        description: "Bloom Café is one of the most aesthetically pleasing spots for a sip in town. The baristas at Bloom are serving up locally roasted coffee from 1645 Coffee Roasters, that pairs perfectly with a house-made pastry. Dishes are seasonally-inspired and full of locally-sourced ingredients cooked over hot coals.",
+        coverImg: "bloom.png",
+        stats: {
+            rating: 4.7,
+            reviewCount: 133,
+        },
+        location: "38 Winwood Street, Thebarton",
+        facilities: ["Alfresco/Outdoor Dining", "Carpark", "Family Friendly", "Takeaway Available", "Pet Friendly - Enquire", "Lawn / Gardens", "Private Dining Area", "Entertainment", "Non Smoking", "Suitable for Functions"],
+        menu: [
+            {
+                name: "Bloom Burger",
+                image: "bloom-burger.png",
+                ingredients: "Bap roll, beef sausage, scrambled egg, american cheese, mustard aioli",
+                price: "19",
+            },
+            {
+                name: "Cherry Chocolate Cake",
+                image: "cherry-chocolate-cake.png",
+                ingredients: "Chocolate torte, cherry, creme fraiche",
+                price: "16",      
+            },
+            {
+                name: "White Balsamic Vinaigrette",
+                image: "white-balsamic-vinaigrette.png",
+                ingredients: "Leafy greens, white balsamic",
+                price: "9",      
+            }
+        ]
     },
 ]
