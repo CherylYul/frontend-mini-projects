1951856da8565dfdb6c0bf9e2cf6e3cb4b4b31b7
ycheryl <yulcheryl2357@gmail.com>
2024-01-17 11:32:45 +0700

    create footer

---
+++ website/coffee-shop/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <h3 className="footer">
+            <i className="bi bi-emoji-heart-eyes"></i>
+            ENJOY YOUR TIME IN ADELAIDE! 
+            <i className="bi bi-arrow-through-heart"></i>
+        </h3>
+    )
+}
