13683ece9c24b9277b51be1c41d9b00956447ca6
ycheryl <yulcheryl2357@gmail.com>
2024-01-18 07:51:22 +0700

    update with react

---
+++ website/nft-site/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./pages/header"
+import SessionOne from "./pages/SessionOne"
+import SessionTwo from "./pages/SessionTwo"
+import Footer from "./pages/Footer"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <SessionOne />
+            <SessionTwo />
+            <Footer />
+        </div>
+    )
+}
