a0875054a18211ff1b6925b904dede7e1be4cdcc
ycheryl <yulcheryl2357@gmail.com>
2024-01-20 00:07:26 +0700

    update color scheme with react

---
+++ auto-generator/color-scheme/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Header from "./components/Header"
+import Scheme from "./components/Scheme"
+const FormContext = React.createContext()
+export { FormContext }
+
+export default function App() {
+    const [data, setData] = React.useState(["#BD2705", "#F03005", "#FC522C", "#FD7B5D", "#FEA48F"])
+    const [form, setForm] = React.useState({
+        color: "#FF6542",
+        mode: "monochrome",
+    })
+    return (
+        <div>
+            <FormContext.Provider value={{form, setForm, data, setData}}>
+                <Header />
+                <Scheme />
+            </FormContext.Provider>
+        </div>
+    )
+}
