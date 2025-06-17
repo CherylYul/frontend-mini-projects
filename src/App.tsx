00e1ff240493597089f65ecb360d71b703b9d0b8
ycheryl <yulcheryl2357@gmail.com>
2025-06-17 08:17:21 +0700

    add tsc + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ImageCarousel from "../components/ImageCarousel";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <ImageCarousel />
+    </>
+  );
+}
+
+export default App;
