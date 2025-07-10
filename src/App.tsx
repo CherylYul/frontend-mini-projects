0c90e04913820f6f60999765ea9eeec80590c106
ycheryl <yulcheryl2357@gmail.com>
2025-07-10 16:17:02 +0700

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import ImageCarousel from "../components/ImageCarousel";
 
+const images = [
+  {
+    src: "https://picsum.photos/id/600/600/400",
+    alt: "Forest",
+  },
+  {
+    src: "https://picsum.photos/id/100/600/400",
+    alt: "Beach",
+  },
+  {
+    src: "https://picsum.photos/id/200/600/400",
+    alt: "Yak",
+  },
+  {
+    src: "https://picsum.photos/id/300/600/400",
+    alt: "Hay",
+  },
+  {
+    src: "https://picsum.photos/id/400/600/400",
+    alt: "Plants",
+  },
+  {
+    src: "https://picsum.photos/id/500/600/400",
+    alt: "Building",
+  },
+];
+
 function App() {
   return (
-    <div className="mx-auto flex w-2xl flex-col items-center justify-center bg-amber-400">
-      <h1>Vite + React</h1>
-      <ImageCarousel />
+    <div className="mx-auto max-w-2xl px-4 pt-16">
+      <ImageCarousel images={images} />
     </div>
   );
 }
