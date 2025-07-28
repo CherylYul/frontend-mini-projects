29d08c90e2f78cbfb429f389f24743718a89fe56
ycheryl <yulcheryl2357@gmail.com>
2025-07-29 00:26:45 +0700

    move hooks and components to src

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
-import ImageCarouselOptimize from "../components/ImageCarousel/ImageCarouselOptimize";
-
-const images = [
-  {
-    src: "https://picsum.photos/id/600/600/400",
-    alt: "Forest",
-  },
-  {
-    src: "https://picsum.photos/id/100/600/400",
-    alt: "Beach",
-  },
-  {
-    src: "https://picsum.photos/id/200/600/400",
-    alt: "Yak",
-  },
-  {
-    src: "https://picsum.photos/id/300/600/400",
-    alt: "Hay",
-  },
-  {
-    src: "https://picsum.photos/id/400/600/400",
-    alt: "Plants",
-  },
-  {
-    src: "https://picsum.photos/id/500/600/400",
-    alt: "Building",
-  },
-];
-
 function App() {
   return (
     <div className="mx-auto max-w-2xl px-4 pt-16">
-      <ImageCarouselOptimize images={images} />
+      {/* <ImageCarouselOptimize images={images} /> */}
     </div>
   );
 }
---
+++ src/constants/images.constant.ts
@@ -0,0 +1,31 @@
+type ImageProps = {
+  src: string;
+  alt: string;
+};
+
+export const images: ImageProps[] = [
+  {
+    src: "https://picsum.photos/id/600/600/400",
+    alt: "Forest",
+  },
+  {
+    src: "https://picsum.photos/id/100/600/400",
+    alt: "Beach",
+  },
+  {
+    src: "https://picsum.photos/id/200/600/400",
+    alt: "Yak",
+  },
+  {
+    src: "https://picsum.photos/id/300/600/400",
+    alt: "Hay",
+  },
+  {
+    src: "https://picsum.photos/id/400/600/400",
+    alt: "Plants",
+  },
+  {
+    src: "https://picsum.photos/id/500/600/400",
+    alt: "Building",
+  },
+];
